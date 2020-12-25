57c34c0b0ef2ed1e3529bcd90abfda989d27ea02
Jan Skowronski <crianonim@gmail.com>
2020-12-25 18:24:08 +0000

    get list of monster headers

--- src/Main.elm
+++ src/Main.elm
@@ -5,6 +5,8 @@ import Dict
 import Html exposing (Attribute, Html, a, button, div, h1, hr, img, input, p, pre, text)
 import Html.Attributes exposing (selected, src, value)
 import Html.Events exposing (onClick, onInput)
+import Http
+import Json.Decode exposing (Decoder, field, string)
 import Platform.Cmd exposing (Cmd)
 
 
@@ -18,30 +20,67 @@ main =
         }
 
 
+toApi url =
+    "https://www.dnd5eapi.co" ++ url
+
+
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( {}
-    , Cmd.none
+    ( { monstersList = [] }
+    , Http.get
+        { url = toApi "/api/monsters"
+        , expect = Http.expectJson GotMonstersList monstersDecoder
+        }
     )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        _ ->
-            ( model, Cmd.none )
+        GotMonstersList result ->
+            case result of
+                Err x ->
+                    let
+                        y =
+                            Debug.log "Errr" x
+                    in
+                    ( model, Cmd.none )
+
+                Ok monstersList ->
+                    ( { model | monstersList = monstersList }, Cmd.none )
 
 
 type alias Model =
-    {}
+    { monstersList : List MonsterHeader }
+
+
+type alias MonsterHeader =
+    { index : String
+    , name : String
+    , url : String
+    }
 
 
 type Msg
-    = Noop
+    = GotMonstersList (Result Http.Error (List MonsterHeader))
+
+
+monstersDecoder : Decoder (List MonsterHeader)
+monstersDecoder =
+    field "results"
+        (Json.Decode.list monsterDecoder)
+
+
+monsterDecoder : Decoder MonsterHeader
+monsterDecoder =
+    Json.Decode.map3 MonsterHeader
+        (field "index" string)
+        (field "name" string)
+        (field "name" string)
+
 
 
 -- SUBSCRIPTIONS
-
 
 
 subscriptions : Model -> Sub Msg
@@ -55,4 +94,11 @@ subscriptions model =
 
 view : Model -> Html Msg
 view model =
-    div [] []
+    div []
+        [ div [] (List.map viewMonsterHeader model.monstersList)
+        ]
+
+
+viewMonsterHeader : MonsterHeader -> Html msg
+viewMonsterHeader { index, name, url } =
+    p [ Html.Attributes.title index ] [ text name ]
